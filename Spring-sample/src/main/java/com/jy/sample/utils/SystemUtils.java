4000297cbccd717d0d4fb2c31dba826b36ffbe32
wody-crew <wody-crew@wody-crewmate>
2016-09-12 14:42:36 +0900

    utils

--- Spring-sample/src/main/java/com/jy/sample/utils/SystemUtils.java
+++ Spring-sample/src/main/java/com/jy/sample/utils/SystemUtils.java
@@ -1,5 +1,8 @@
 package com.jy.sample.utils;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 /**
  * Application 전반적인 유틸리티 클래스.
  * @author wody
@@ -13,15 +16,34 @@ public class SystemUtils {
 	public final static String STRING_FALSE = "N";
 	
 	/** Exception stacktrace 최대 출력 라인  */
-	private final static int STACKTRACE_MAX_LINE = 10;
+	public final static int STACKTRACE_MAX_LINE = 10;
 	
 	/** 개행 문자열 */
-	private final static String STR_LINE_FEED = "\r\n";
+	public final static String STR_LINE_FEED = "\r\n";
+	
+	
+	/** OS에 따른 문자열 구분자. 생성자에서 초기화 된다. */
+	public static final String STRING_LINE_SEPARATOR;
+
+	/** OS에 따른 디렉토리 구분자. 생성자에서 초기화 된다. */
+	public static final String STRING_FILE_SEPARATOR;
+	
+	/** 문자열 구분자 및 디렉토리 구분자 지정 */
+	static {
+		String lineSeparator = System.getProperty("line.separator");
+		String fileSeparator = System.getProperty("file.separator");
+		if (lineSeparator == null) {
+			lineSeparator = "\n";
+		}
+		
+		STRING_LINE_SEPARATOR = lineSeparator;
+		STRING_FILE_SEPARATOR = fileSeparator;
+	}
 	
 	/**
 	 * true -> "Y", false -> "N"
-	 * @param val
-	 * @return
+	 * @param val boolean value.
+	 * @return "Y" or "N"
 	 */
 	public final static String changeStrBoolean(final boolean val) {
 		return val == true ? SystemUtils.STRING_TRUE : SystemUtils.STRING_FALSE;
@@ -29,7 +51,8 @@ public class SystemUtils {
 	
 	/**
 	 * "Y" -> true, "N" -> false
-	 * @return
+	 * @param string value
+	 * @return true or false
 	 */
 	public final static boolean changeStrBoolean(final String val) {
 		if (SystemUtils.STRING_TRUE.equals(val)) {
@@ -41,8 +64,8 @@ public class SystemUtils {
 	
 	/**
 	 * true -> 1, false -> 2
-	 * @param val
-	 * @return
+	 * @param val boolean value
+	 * @return 1 or 0
 	 */
 	public final static int changeIntBoolean(final boolean val) {
 		return val == true ? 1 : 0;
@@ -50,8 +73,8 @@ public class SystemUtils {
 	
 	/**
 	 * 1 -> true, 0 -> false
-	 * @param val
-	 * @return
+	 * @param val integer value
+	 * @return true or false
 	 */
 	public final static boolean changeIntBoolean(final int val) {
 		if (val == 1) {
@@ -101,5 +124,28 @@ public class SystemUtils {
 		}
 		
 		return eBuilder.toString();
+	}
+	
+	/**
+	 * Object를 문자열로 표시함.
+	 * @param object 문자열로 표시할 object
+	 * @see ToStringStyle 스타일 지정 객체
+	 * @see ToStringBuilder String 변환 클래스
+	 * @return string for Object
+	 */
+	public static String toStringObject(final Object object) {
+		return SystemUtils.toStringObject(object, ToStringStyle.DEFAULT_STYLE);
+	}
+	
+	/**
+	 * Object를 문자열로 표시함.
+	 * @param object 문자열로 표시할 object
+	 * @param style ToStringStyle의 스타일
+	 * @see ToStringStyle 스타일 지정 객체
+	 * @see ToStringBuilder String 변환 클래스
+	 * @return string for Object
+	 */
+	public static String toStringObject(final Object object, final ToStringStyle style) {
+		return ToStringBuilder.reflectionToString(object, style);
 	}
 }
